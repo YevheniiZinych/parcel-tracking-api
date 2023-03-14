4a1ee75669435fadede09d92ec5acde9a855fea3
ZEugenii <zinich911@gmail.com>
2023-03-14 14:10:18 +0200

    create controller for get ofices by city name

--- model/office.js
+++ model/office.js
@@ -5,30 +5,17 @@ const { handleMongooseError } = require("../helpers");
 
 const officeSchema = new Schema(
   {
-    city: {
-      type: String,
-      required: [true, "City name is required"],
-    },
-    //     office: {
-    //       type: String,
-    //       required: [true],
-    //     },
-    //     number: {
-    //       type: String,
-    //       required: [true],
-    //     },
+    description: [{}],
   },
   { versionKey: false }
 );
 
 officeSchema.post("save", handleMongooseError);
 
-const Office = model("office", officeSchema);
+const Office = model("offices", officeSchema);
 
 const findOfficeSchema = Joi.object({
   city: Joi.string().required(),
-  //   office: Joi.string().required(),
-  //   number: Joi.string().required(),
 });
 
 module.exports = {
