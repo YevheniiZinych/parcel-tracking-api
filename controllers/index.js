a021be080d173366afb1184920c34c656ca3513a
ZEugenii <zinich911@gmail.com>
2023-08-03 16:03:55 +0300

    fix re export

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const { getAllOffices } = require("./offices");
 const { getInvoiceNumber } = require("./invoices");
+const { getAllOffices } = require("./offices");
 
 module.exports = { getAllOffices, getInvoiceNumber };
